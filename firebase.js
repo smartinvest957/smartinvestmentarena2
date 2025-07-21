22d1f1433cbc19e694b82f7df517c41f6805991d
smartinvest957 <smartinvestarena@gmail.com>
2025-07-21 08:53:15 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
 
 <script>
   const firebaseConfig = {
-    apiKey: "AIzaSyB9nF_x45RPL4qN_O1xldhNhr_4AM5bXIg",
-    authDomain: "smart-invest-tracker.firebaseapp.com",
-    projectId: "smart-invest-tracker",
-    storageBucket: "smart-invest-tracker.appspot.com",
-    messagingSenderId: "464698438524",
-    appId: "1:464698438524:web:fa266eb1b29604bb8a6af9",
-    measurementId: "G-4YXZ0N9D5C"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "smart-invest-tracker",  // Use your actual Firebase project ID
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_ID",
+    appId: "YOUR_APP_ID"
   };
-
-  firebase.initializeApp(firebaseConfig);
+  const app = firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
 </script>
